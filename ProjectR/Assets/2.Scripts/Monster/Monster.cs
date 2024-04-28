ef202d3e81b2ca3d23ec7506fefba0030c36e8c8
changwoomin <changwoomin426@gmail.com>
2024-04-28 23:48:18 +0900

    feat : 몬스터 스크립트 작업 중...
    학교가서 작업할 예정이니 서버에 올림

---
+++ ProjectR/Assets/2.Scripts/Monster/Monster.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Monster : MonoBehaviour
+{
+    public virtual void Attack()
+    {
+        
+    }
+
+    public virtual void Move()
+    {
+        
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        
+    }
+
+    public virtual void Die()
+    {
+        
+    }
+    
+}
