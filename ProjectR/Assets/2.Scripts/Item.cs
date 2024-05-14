8970156ba2b303d6f0bec2cef336247fa89f7329
changwoomin <changwoomin426@gmail.com>
2024-05-14 21:30:21 +0900

    feat : 폭탄 열매 로직 구현

--- ProjectR/Assets/2.Scripts/Item.cs
+++ ProjectR/Assets/2.Scripts/Item.cs
@@ -11,7 +11,8 @@ public class Item : MonoBehaviour
         Mp,
         Elixir,
         Shield,
-        Exp 
+        Exp,
+        Bomb
     }
     
     public EType type;
@@ -27,7 +28,10 @@ public class Item : MonoBehaviour
 
     private void Update()
     {
-        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+        if (type != EType.Bomb)
+        {
+            transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,23 +42,60 @@ public class Item : MonoBehaviour
             {
                 case EType.Hp:
                     //other.GetComponent<PlayerController>().Hp += value;
+                    Destroy(gameObject);
                     break;
                 case EType.Mp:
                     //other.GetComponent<PlayerController>().Mp += value;
+                    Destroy(gameObject);
                     break;
                 case EType.Elixir:
                     //other.GetComponent<PlayerController>().Elixir += value;
+                    Destroy(gameObject);
                     break;
                 case EType.Shield:
                     //other.GetComponent<PlayerController>().Shield += value;
+                    Destroy(gameObject);
                     break;
                 case EType.Exp:
                     //other.GetComponent<PlayerController>().Exp += value;
+                    Destroy(gameObject);
+                    break;
+                case EType.Bomb:
+                    StartCoroutine(Explosion());
                     break;
                 default:
                     print("????");
                     break;
             }
         }
+    }
+
+    private IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(1f);
+        int layerMask = LayerMask.GetMask("Enemy", "Player");
+        RaycastHit [] rayHits =Physics.SphereCastAll(transform.position,15,Vector3.up,0f,layerMask);
+
+        foreach (RaycastHit hitObj in rayHits)
+        {
+            if (hitObj.transform.CompareTag("Warrior"))
+            {
+                hitObj.transform.GetComponent<WarriorPresenter>().TakeDamage(value);
+            }
+            else if (hitObj.transform.CompareTag("Archer"))
+            {
+                hitObj.transform.GetComponent<ArcherPresenter>().TakeDamage(value);
+            }
+            else if (hitObj.transform.CompareTag("Shield")) 
+            {
+                hitObj.transform.GetComponent<ShieldPresenter>().TakeDamage(value);
+            }
+            else if (hitObj.transform.CompareTag("Player")) 
+            {
+                // hitObj.transform.GetComponent<PlayerController>().TakeDamage(10f);
+            }
+        }
+        Destroy(gameObject);
+        
     }
 }
