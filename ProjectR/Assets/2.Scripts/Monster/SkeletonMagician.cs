5ccf73c0c8f9852b271818148601a5e46a69d1ca
Woomin <changwoomin426@gmail.com>
2024-05-25 00:20:22 +0900

    feat : 마법사 해골 프리팹 추가

---
+++ ProjectR/Assets/2.Scripts/Monster/SkeletonMagician.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMagician : Enemy
+{
+    [SerializeField] private GameObject fireBall;
+    [SerializeField] private Transform firePos;
+    
+    
+    protected override IEnumerator AttackPlayer()
+    {
+        Animator.SetBool(Attack,true);
+        IsChase = false;
+        IsAttack = true;
+        yield return new WaitForSeconds(0.5f);
+        GameObject instanceFireBall = Instantiate(fireBall, firePos.position, firePos.rotation);
+        Rigidbody fireBallRb = instanceFireBall.GetComponent<Rigidbody>();
+        fireBallRb.velocity = firePos.forward * 10f;
+        yield return new WaitForSeconds(1.2f);
+        IsChase = true;
+        IsAttack = false;
+        Animator.SetBool(Attack,false);
+        Animator.SetBool(Chase,true);
+    }
+}
